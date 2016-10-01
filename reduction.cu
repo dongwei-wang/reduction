b2a63f94c3e852460146d3f0daae0f973134100b
Dongwei <wdw828@gmail.com>
2016-10-01 18:24:41 -0400

    Oct 1st

--- reduction.cu
+++ reduction.cu
@@ -2,11 +2,10 @@
 #include "reduction_kernel.cu"
 
 #define UPPER_BOUND 1000
-//#define ELEMENTS_PER_BLOCK 2048
 #define BLOCK_SIZE 1024
 
 void runTest();
-int* arrayInit(int len);
+int* arrayInit(unsigned len);
 int computeOnDevice(int* h_data, int array_mem_size);
 int computeGold( int* data, const int len);
 
@@ -17,15 +16,13 @@ int main(){
 
 void runTest(){
 	printf("Please input the number of elements in the array: \n");
-	int num_elements = 4096;
+	unsigned num_elements=4096;
 	/* if( scanf("%d", &num_elements) != 1){ */
 	/*     printf("Input Failed\n"); */
 	/*     return; */
 	/* } */
 
 	int* h_data = arrayInit(num_elements);
-
-
 	int reference = computeGold(h_data, num_elements);
 	int result = computeOnDevice(h_data, num_elements);
 	printf( "Test %s !!!\n", (reference == result) ? "PASSED" : "FAILED");
@@ -34,9 +31,9 @@ void runTest(){
 	return;
 }
 
-int* arrayInit(int len){
-	int* data = (int*) malloc( sizeof( int ) * len );
-	for( int i=0; i<len; i++ ){
+int* arrayInit(unsigned len){
+	int* data = (int*) malloc(sizeof(int)*len);
+	for( unsigned i=0; i<len; i++ ){
 		//data[i] = rand()%UPPER_BOUND;
 		data[i] = 1;
 	}
@@ -45,24 +42,23 @@ int* arrayInit(int len){
 
 int computeOnDevice(int* h_data, int len){
 	int* d_data = NULL;
-	int depth = ceil(log2((double)len));
-	int block_cnt = (pow(2, depth-1)+BLOCK_SIZE-1)/BLOCK_SIZE;
-//	int result;
+	unsigned block_cnt = ((len>>1) + BLOCK_SIZE - 1)/BLOCK_SIZE;
 
-	//int block_cnt = (len+1)/(2*BLOCK_SIZE);
 	printf("The length is %d\n", len);
-	printf("The depth  is %d\n", depth);
 	printf("The block  is %d\n", block_cnt );
+
+	dim3 gridDim(block_cnt, 1);
+	dim3 blockDim(BLOCK_SIZE, 1);
 
 	cudaMalloc((void**)&d_data, len * sizeof(int));
 	cudaMemcpy(d_data, h_data, len * sizeof(int), cudaMemcpyHostToDevice);
-	reduction1<<<block_cnt, BLOCK_SIZE>>>(d_data, depth, len );
+	reduction<<<gridDim, blockDim>>>(d_data, len);
 	cudaMemcpy(h_data, d_data, len * sizeof(int), cudaMemcpyDeviceToHost);
 	cudaFree(d_data);
 	return h_data[0];
 }
 
-int computeGold( int* data, const int len){
+int computeGold( int* data, int len){
 	int total_sum = 0;
 	for( int i = 0; i < len; ++i){
 		total_sum += data[i];
--- reduction_kernel.cu
+++ reduction_kernel.cu
@@ -1,24 +1,12 @@
 // kernel function
-__global__ void reduction1(int *data, int depth, int len){
-	int tid = blockDim.x * blockIdx.x + threadIdx.x ;
-	int step;
-	for(int i=0; i<depth; i++){
-		step = pow((double)2, (double)depth-i-1);
-		if(tid<step){
+__global__ void reduction(int *data, unsigned len){
+	unsigned tid = (blockDim.x * blockIdx.x) + threadIdx.x ;
+	unsigned step= len;
+	while( step != 0 ){
+		step = step>>1;
+		if( tid<step){
 			data[tid] += data[tid+step];
 		}
 		__syncthreads();
 	}
 }
-
-/* __global__ void reduction1(int *data, int depth, int len){ */
-/*     int tid = blockIdx.x * blockDim.x + threadIdx.x ; */
-/*     int step=(len+1)/2; */
-/*     while(step){ */
-/*         if( tid <step && tid+step<2*step){ */
-/*             data[tid] += data[tid+step]; */
-/*             step = (step+1)/2; */
-/*         } */
-/*         __syncthreads(); */
-/*     } */
-/* } */
